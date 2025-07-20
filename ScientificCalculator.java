7c553ac6af647fbb37dbfb7cf2431d0f0c1da2b3
mozaamur <s135058@student.squ.edu.om>
2025-07-20 14:25:45 +0400

    Addition DoneSubtraction, multiplocation, division all done

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -21,9 +21,18 @@ public class ScientificCalculator {
         double addResult = num1 + num2;
         return addResult; // Placeholder
     }
-    // public static double subtract(...) { ... }
-// public static double multiply(...) { ... }
-// public static double divide(...) { ... }
+    public static double subtract(double num1, double num2) {
+        double subResult = num1 + num2;
+        return subResult;
+    }
+    public static double multiply(double num1, double num2) {
+        double multiResult = num1 * num2;
+        return multiResult;
+    }
+    public static double divide(double num1, double num2) {
+        double divideResult = num1 / num2;
+        return divideResult;
+    }
 // --- Scientific Math Methods ---
 // public static double calculateSquareRoot(...) { ... }
 // public static double calculatePower(...) { ... }
@@ -43,11 +52,49 @@ public class ScientificCalculator {
             System.out.println(result);
         }catch (InputMismatchException e) {
             System.out.println("Try again");
-        }
-
-// Implement try-catch for InputMismatchException here!
-
+        }// Implement try-catch for InputMismatchException here!
     }
-// private static void performSubtraction(Scanner scanner) { ... }
+    private static void performSubtraction(Scanner scanner) {
+        try {
+            // Prompt for first number
+            double num1 = scanner.nextDouble();
+// Prompt for second number
+            double num2 = scanner.nextDouble();
+// Call add() method
+            double result =subtract(num1, num2);
+// Print result
+            System.out.println(result);
+        }catch (InputMismatchException e) {
+            System.out.println("Try again");
+        }
+    }
+    private static void performMultiplication(Scanner scanner) {
+        try {
+            // Prompt for first number
+            double num1 = scanner.nextDouble();
+// Prompt for second number
+            double num2 = scanner.nextDouble();
+// Call add() method
+            double result =multiply(num1, num2);
+// Print result
+            System.out.println(result);
+        }catch (InputMismatchException e) {
+            System.out.println("Try again");
+        }
+    }
+    private static void performDivision(Scanner scanner) {
+        try {
+            // Prompt for first number
+            double num1 = scanner.nextDouble();
+// Prompt for second number
+            double num2 = scanner.nextDouble();
+// Call add() method
+            double result =divide(num1, num2);
+// Print result
+            System.out.println(result);
+        }catch (InputMismatchException e) {
+            System.out.println("Try again");
+        }
+    }
 // ... and so on for all required functions
 }
